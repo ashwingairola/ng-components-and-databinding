13c47cde8bea668d1acbca65cf57cbebcee450b8
Ashwin <ashwingairola999@gmail.com>
2021-06-07 15:33:27 +0530

    64. Splitting Apps into Components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	serverElements: any[] = [];
-	newServerName = '';
-	newServerContent = '';
-
-	onAddServer() {
-		this.serverElements.push({
-			type: 'server',
-			name: this.newServerName,
-			content: this.newServerContent
-		});
-	}
-
-	onAddBlueprint() {
-		this.serverElements.push({
-			type: 'blueprint',
-			name: this.newServerName,
-			content: this.newServerContent
-		});
-	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, CockpitComponent, ServerElementComponent],
 	imports: [BrowserModule, AppRoutingModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent]
